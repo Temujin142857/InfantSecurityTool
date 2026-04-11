3d2cdb458539fe1c23704f01891a7f2ced3a3d19
Yati03 <yanngiffaux@yahoo.com>
2026-04-11 19:12:23 -0400

    fixing merging conflict

--- main_apr3a/Mpu6050.cpp
+++ main_apr3a/Mpu6050.cpp
@@ -2,29 +2,58 @@
 #include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+#include <math.h>
 
 Adafruit_MPU6050 mpu;
+static bool mpu_ready = false;
 
-void mpu_init(TwoWire *Wire) {
-  delay(100);
+void mpu_init(TwoWire *wire) {
+    delay(500);
 
-  if (!mpu.begin(0x68, Wire)) {
-    Serial.println("Failed to find MPU6050 chip");
-    
-  }else{
-    Serial.println("MPU6050 Found!");
+    Serial.println("Initializing MPU6050...");
 
- }
+    if (mpu.begin(0x68, wire)) {
+        Serial.println("MPU6050 found at 0x68");
+        mpu_ready = true;
+    }
+    else if (mpu.begin(0x69, wire)) {
+        Serial.println("MPU6050 found at 0x69");
+        mpu_ready = true;
+    }
+    else {
+        Serial.println("ERROR: MPU6050 not found at 0x68 or 0x69");
+        mpu_ready = false;
+        return;
+    }
+
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+    Serial.println("MPU6050 ready!");
 }
 
 void readMotion(float *motion) {
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+    if (!mpu_ready) {
+        Serial.println("MPU not ready");
+        *motion = 0;
+        return;
+    }
 
-  float current = fabs(a.acceleration.x) + fabs(a.acceleration.y) + fabs(a.acceleration.z);
-  static float last = 0;
-  float delta = fabs(current - last);
-  last = current;
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    
+	Serial.printf("x early: %f %\n", a.acceleration.x );	
+    Serial.printf("y early: %f %\n", a.acceleration.y );
+    Serial.printf("z early: %f %\n", a.acceleration.z );
+    float z=a.acceleration.z+10.8;
+    float y=a.acceleration.y+0.1;
+    float x=a.acceleration.x-1.5;
 
-  *motion = delta;
+
+    *motion = sqrt(
+        x * x +
+        y * y +
+        z * z
+    );
 }
